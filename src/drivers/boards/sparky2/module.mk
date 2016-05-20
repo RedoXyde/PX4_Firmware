5761fad51992f40c6204fa3d91f8c92f583a7a6a
RedoX <git@redox.ws>
2016-05-20 17:35:46 +0200

    [Sparky2] Cleanup, MTD

--- src/drivers/boards/sparky2/module.mk
+++ src/drivers/boards/sparky2/module.mk
@@ -2,11 +2,11 @@
 # Board-specific startup code for the PX4FMU
 #
 
-SRCS		 = px4fmu_can.c \
-		   px4fmu_init.c \
-		   px4fmu_pwm_servo.c \
-		   px4fmu_spi.c \
-		   px4fmu_usb.c \
-		   px4fmu_led.c
+SRCS		 = sparky2_can.c \
+		   sparky2_init.c \
+		   sparky2_pwm_servo.c \
+		   sparky2_spi.c \
+		   sparky2_usb.c \
+		   sparky2_led.c
 
 MAXOPTIMIZATION	 = -Os
